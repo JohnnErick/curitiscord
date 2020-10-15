ba39ce6b3c2dd66f07506749464c0d4ae2bd7997
Nathan Heffren <46766105+nathanheffren@users.noreply.github.com>
2020-10-15 20:02:02 -0300

    :sparkles:(code): Sidebar menu (#2)
    - Create sidebar component
    - Add svg icons (logo and plus)
    - Create menu items render function

--- src/Components/SideMenu/index.jsx
+++ src/Components/SideMenu/index.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container } from './styles'
+import Logo from 'Components/Icons/Logo.jsx';
+import Plus from 'Components/Icons/Plus.jsx';
+
+import {
+  Container,
+  ContentWrapper,
+  ButtonWrapper,
+  Button,
+  Divisor,
+  LogoBox,
+  ActionIcon,
+  ServerIcon
+} from './styles';
 
 export default function SideMenu() {
+  const [ active, setActive ] = useState('0-0');
+
+  const src = 'https://cdn.discordapp.com/icons/670106577533141002/c6fb757a7cbebbff63a180cd23782421.png?size=128';
+
+  const menuItens = [{
+    items: [
+      {
+        content: <Logo width="28" />,
+        link: '',
+      }
+    ]
+  }, {
+    items: [
+      {
+        serverImg: src,
+        link: '',
+      },{
+        serverImg: src,
+        link: '',
+      },{
+        serverImg: src,
+        link: '',
+      },{
+        content: <Plus width="24" />,
+        link: '',
+      }
+    ],
+  }, {
+    items: [
+      {
+        content: <Logo width="28" />,
+        link: '',
+      }
+    ],
+  }];
+
   return (
     <Container>
-      Menu
+      {menuItens.map(({ items }, index1) => (
+        <ContentWrapper key={`menu-group-${index1}`}>
+          {items.map(({ content, link, serverImg }, index2) => (
+            <ButtonWrapper
+              key={`buttom-item-${index2}`}
+              active={active === `${index1}-${index2}`}
+              onClick={() => setActive(`${index1}-${index2}`)}
+            >
+              <Button background={serverImg}>
+                {content && content}
+              </Button>
+              <span />
+            </ButtonWrapper>
+          ))}
+        </ContentWrapper>
+      ))}
     </Container>
   );
 };
